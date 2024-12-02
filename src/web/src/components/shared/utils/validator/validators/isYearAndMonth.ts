7eb999eb120b84a74568d82465473aa977ed3328
Efim Tuhvatullin <efimtuhvatullin@gmail.com>
2024-12-02 20:25:52 +0300

    feat: validators/IsYearAndMont

--- src/web/src/components/shared/utils/validator/validators/isYearAndMonth.ts
+++ src/web/src/components/shared/utils/validator/validators/isYearAndMonth.ts
@@ -6,19 +6,12 @@ export const isYearAndMonth: GetValidator<never, string> =
 
 		if (array.length === 2) {
 			const month = Number(array[0]);
-
-			if (month < 1 || month > 12) {
-				return 'Некорректный месяц';
-			}
-
 			const year = Number(array[1]);
 
-			if (year < 2021 || year > 2026) {
-				return 'Некорректный год';
+			if (month >= 1 && month <= 12 && year >= 2021 && year <= 2026) {
+				return null;
 			}
-
-			return null;
 		}
 
-		return 'Некорректный дата';
+		return 'Поле от 1 до 12, второе от 21 до 26';
 	};
