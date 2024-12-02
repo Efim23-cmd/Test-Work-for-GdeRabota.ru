eb640605a9b4ea84428c32a72beea472fd21f105
Efim Tuhvatullin <efimtuhvatullin@gmail.com>
2024-12-02 20:31:00 +0300

    feat: validators/IsNumberCard

--- src/web/src/components/shared/utils/validator/validators/isNumberCard.ts
+++ src/web/src/components/shared/utils/validator/validators/isNumberCard.ts
@@ -2,10 +2,14 @@ import { GetValidator } from '../type';
 
 export const isNumberCard: GetValidator<never, string> =
 	() => async (value) => {
-		const number = BigInt(value.replace(/\D+/g, ''));
+		const number = Number(value.replace(/\D+/g, ''));
 
-		if (!Number.isNaN(number) && number >= 13 && number <= 13) {
-			return null;
+		if (!Number.isNaN(number)) {
+			const numberString = number.toString();
+
+			if (numberString.length >= 13 && numberString.length <= 19) {
+				return null;
+			}
 		}
 
 		return 'Поле от 13 до 19 числовых символов';
