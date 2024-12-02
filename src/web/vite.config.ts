cbbcc692459269d546ef0e13fc6e7a3ec06b3095
Efim Tuhvatullin <efimtuhvatullin@gmail.com>
2024-12-02 20:26:18 +0300

    feat: add svgr plugin

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from "vite-plugin-svgr";
+
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
 			'@styles': resolve(__dirname, './src/components/shared/styles'),
